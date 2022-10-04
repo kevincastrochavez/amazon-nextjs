59dba692ab056a1ce39b33a55882d66370fa7aea
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-04 00:06:40 -0600

    finished bottom navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
-import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
 
 function Header() {
   return (
@@ -46,6 +50,23 @@ function Header() {
             </p>
           </div>
         </div>
+      </div>
+
+      <div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
+        <p className='flex link items-center'>
+          <MenuIcon className='h-6 mr-1' />
+          All
+        </p>
+
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Business</p>
+        <p className='link'>Today's Deals</p>
+        <p className='link hidden lg:inline-flex '>Electronics</p>
+        <p className='link hidden lg:inline-flex '>Food & Grocery</p>
+        <p className='link hidden lg:inline-flex '>Prime</p>
+        <p className='link hidden lg:inline-flex '>Buy Again</p>
+        <p className='link hidden lg:inline-flex '>Shopper Toolkit</p>
+        <p className='link hidden lg:inline-flex '>Health & Personal Care</p>
       </div>
     </header>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,8 @@ export default function Home() {
       </Head>
 
       <Header />
+
+      <main className='max-w-screen-2xl mx-auto'></main>
     </div>
   );
 }
