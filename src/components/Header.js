ee1df7121f000f28f6ff4a85493a95e04db3cc40
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-03 23:59:24 -0600

    built right section of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/outline';
+import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
 
 function Header() {
   return (
@@ -23,9 +23,30 @@ function Header() {
           />
           <SearchIcon className='h-12 p-4' />
         </div>
-      </div>
 
-      <div></div>
+        <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+          <div className='link'>
+            <p>Hello Kevin Castro</p>
+            <p className='font-extrabold md:text-sm'>Acoount & Lists</p>
+          </div>
+
+          <div className='link'>
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>& Orders</p>
+          </div>
+
+          <div className='relative link flex items-center'>
+            <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
+              0
+            </span>
+
+            <ShoppingCartIcon className='h-10' />
+            <p className='hidden md:inline font-extrabold md:text-sm mt-2'>
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
