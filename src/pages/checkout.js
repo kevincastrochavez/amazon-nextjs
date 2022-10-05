76178d9cfc3173c81bf3d891ffcbdaa04b9a9462
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-05 13:41:10 -0600

    added left side of checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 import Header from '../components/Header';
 
@@ -8,6 +9,21 @@ function Checkout() {
       <Header />
 
       <h1>Checkout Page</h1>
+
+      <main className='lg:flex max-w-screen-2xl mx-auto'>
+        <div className='flex-grow m-5 shadow-sm'>
+          <Image
+            src='https://links.papareact.com/ikj'
+            width={1020}
+            height={250}
+            objectFit='contain'
+          />
+
+          <div className='flex flex-col p-5 space-y-10 bg-white'>
+            <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
