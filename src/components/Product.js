c86cfcdaeb770cd8a2e9ed933e6a442d5fcb24b1
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-05 17:10:58 -0600

    implemented functionality for adding and removing items from the basket using redux

--- src/components/Product.js
+++ src/components/Product.js
@@ -19,7 +19,16 @@ function Product({ id, title, price, description, category, image }) {
   const [hasPrime] = useState(Math.random() < 0.5);
 
   const addItemToBasket = () => {
-    const product = { id, title, price, description, category, image };
+    const product = {
+      id,
+      title,
+      rating,
+      price,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
 
     dispatch(addToBasket(product));
   };
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import Currency from 'react-currency-formatter';
 
 import Header from '../components/Header';
+import { selectItems } from '../slices/basketSlice';
+import CheckoutProduct from '../components/CheckoutProduct';
 
 function Checkout() {
+  const items = useSelector(selectItems);
+
   return (
     <div className='bg-gray-100'>
       <Header />
-
-      <h1>Checkout Page</h1>
 
       <main className='lg:flex max-w-screen-2xl mx-auto'>
         <div className='flex-grow m-5 shadow-sm'>
@@ -20,7 +24,39 @@ function Checkout() {
           />
 
           <div className='flex flex-col p-5 space-y-10 bg-white'>
-            <h1 className='text-3xl border-b pb-4'>Your Shopping Basket</h1>
+            <h1 className='text-3xl border-b pb-4'>
+              {items.length === 0
+                ? 'Your Shopping Basket is empty'
+                : 'Shopping basket'}
+            </h1>
+
+            {items.map(
+              (
+                {
+                  id,
+                  title,
+                  price,
+                  rating,
+                  description,
+                  category,
+                  image,
+                  hasPrime,
+                },
+                i
+              ) => (
+                <CheckoutProduct
+                  key={i}
+                  id={id}
+                  title={title}
+                  price={price}
+                  rating={rating}
+                  description={description}
+                  category={category}
+                  image={image}
+                  hasPrime={hasPrime}
+                />
+              )
+            )}
           </div>
         </div>
       </main>
