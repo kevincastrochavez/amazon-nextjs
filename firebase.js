53613f258319542e0751276462ad9964eb5dd4a7
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-05 13:36:32 -0600

    started building the checkout page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCkNxBCxgAIfDfnZl3UdhMmNw64Ax-4QFg',
+  authDomain: 'fir-80000.firebaseapp.com',
+  projectId: 'fir-80000',
+  storageBucket: 'fir-80000.appspot.com',
+  messagingSenderId: '946473078906',
+  appId: '1:946473078906:web:c94059571bc1b0a467abc3',
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,18 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from '@heroicons/react/outline';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function Header() {
+  const router = useRouter();
+
   return (
     <header>
       <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
         <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
           <Image
+            onClick={() => router.push('/')}
             src='https://links.papareact.com/f90'
             width={150}
             height={40}
@@ -29,7 +34,7 @@ function Header() {
         </div>
 
         <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-          <div className='link'>
+          <div onClick={signIn} className='link cursor-pointer'>
             <p>Hello Kevin Castro</p>
             <p className='font-extrabold md:text-sm'>Acoount & Lists</p>
           </div>
@@ -39,7 +44,10 @@ function Header() {
             <p className='font-extrabold md:text-sm'>& Orders</p>
           </div>
 
-          <div className='relative link flex items-center'>
+          <div
+            onClick={() => router.push('/checkout')}
+            className='relative link flex items-center'
+          >
             <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
               0
             </span>
