2f631fd6a54ea1e1ff9149162fff79dd5f96fca4
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-06 15:07:40 -0600

    added cors config

--- next.config.js
+++ next.config.js
@@ -7,12 +7,24 @@ module.exports = {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
   },
 
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: '/api/create-checkout-session',
-        destination:
-          'https://next-server-eta.vercel.app/api/create-checkout-session',
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
     ];
   },
